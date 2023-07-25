14b662d8edf48c457a4423cc8dd58794272c7a5d
Max Cyprys <mc.cyprysik@icloud.com>
2023-07-25 18:51:00 +0200

    feat: new migration creating a links table

---
+++ migrations/versions/6666e268c7d1_create_table_links.py
@@ -0,0 +1,44 @@
+"""
+
+create_table_links
+
+Revision ID: 6666e268c7d1
+Creation date: 2023-07-25 18:34:32.790850
+
+"""
+from alembic import op, context
+
+
+# revision identifiers, used by Alembic.
+revision = "6666e268c7d1"
+down_revision = "b2d1400908a1"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute(
+        """--sql
+        CREATE TABLE links(
+            lid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+            owner_uid UUID NOT NULL,
+            
+            original_link TEXT NOT NULL,
+            short_link TEXT NOT NULL,
+            protected BOOLEAN NOT NULL, 
+            password TEXT,
+            
+
+            created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+            
+            CONSTRAINT fk_user FOREIGN KEY(owner_uid) REFERENCES users(uid) ON DELETE CASCADE
+        );
+        """
+    )
+
+def downgrade() -> None:
+    op.execute(
+        """--sql
+        DROP TABLE links;
+        """
+    )
