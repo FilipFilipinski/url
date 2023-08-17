175663a46965362d2dbddc94c2412618d55819b9
Max Cyprys <mc.cyprysik@icloud.com>
2023-08-17 18:23:22 +0200

    feat: auth - added login, registration and with_auth

--- src/service/auth/authorization.py
+++ src/service/auth/authorization.py
@@ -1,41 +1,119 @@
-from aiohttp import web
+from uuid import UUID
 
-from src.repos.token_repo import AccessToken
+from aiohttp import web
+from aiohttp.web import HTTPUnauthorized
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
+from src.models.access_token import AccessToken
+from src.models.user import User
+from src.repos.token_repo import AccessTokenRepository
 from src.repos.user_repo import UserRepository
+
+ph = PasswordHasher()
 
 
 class AuthorizationService:
-    def __init__(self, user_repo: UserRepository, token_repo: AccessToken):
+    def __init__(self, user_repo: UserRepository, token_repo: AccessTokenRepository):
         self.user_repo = user_repo
         self.token_repo = token_repo
 
-    async def auth(self, req: web.Request) -> bool:
+    async def login(self, username: str, password: str) -> AccessToken | None:
+        """
+        This method returns create a brand-new token
+        that user can use to access API.
+
+        Also, we should revoke any already generated
+        tokens.
+
+        Parameters:
+          username (str): User's username
+          password (str): User's password
+
+        Returns:
+          access_token (AccessToken): Authorization token
+        """
+
+        user = await self.user_repo.find(username=username)
+        if not user:
+            return None
+
+        # Passwords doesn't match
+        try:
+            ph.verify(user.password, password)
+        except VerifyMismatchError:
+            return None
+
+        token = await self.token_repo.generate(user.uid)
+        return token
+
+    async def register(self, username: str, password: str, email: str) -> AccessToken | None:
+        """
+        Creates a new user record
+        and users token in the database.
+
+        Parameters:
+          username (str): User's username
+          password (str): User's password
+          email (str): User's email
+
+        Returns:
+          access_token (AccessToken): Authorization token
+        """
+
+        user = await self.user_repo.create(email, username, password)
+        if not user:
+            return None
+
+        token = await self.token_repo.generate(user.uid)
+        return token
+
+    async def validate_token(self, token: UUID) -> bool:
+        """
+        This method validates given token based
+        on database appearance of it.
+
+        Parameters:
+          token (UUID): Authorization token
+
+        Returns:
+          result (bool): Validation result
+        """
+
+        token = await self.token_repo.find(token)
+        return bool(token)
+
+    async def with_auth(self, req: web.Request) -> User:
         """
         Validates token that was sent in the request,
-        if the token was valid, returns True.
+        if the token was valid, returns User.
+
+        Throws HTTPUnauthorized in case if token isn't valid.
+
         Parameters:
           req (web.Request): AioHTTP's request object
+
         Returns:
-         bool
+          user (User): User object
         """
 
         auth_header = req.headers.get("Authorization", "")
         auth_segments = auth_header.split(" ")
 
         if len(auth_segments) != 2:
-            return False
+            raise HTTPUnauthorized()
 
         # If first segment isn't a Bearer typed, throw Unauthorized
         if auth_segments[0] != "Bearer":
-            return False
+            raise HTTPUnauthorized()
 
         try:
-            token = str(auth_segments[1])
+            token = UUID(auth_segments[1])
         except ValueError:
-            return False
+            raise HTTPUnauthorized()
 
         access_token = await self.token_repo.find(token)
         if not access_token:
-            return False
+            raise HTTPUnauthorized()
 
-        return True
+        return access_token.user
